f9a23c8227524d66a0004015517aef41af54e457
smzaafir <128362703+smzaafir@users.noreply.github.com>
2024-03-18 14:57:54 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,231 @@
+//Questions array
+            let questions = [
+            {
+                 number:1,
+                 question: "15−3×2+4=",
+                 answer : "13",
+                 options : [
+                                  "16",
+                                  "13",
+                                  "10",
+                                  "8"
+                                ]
+            
+            },
+            {
+                 number:2,
+                 question: "24÷3+6×2=",
+                 answer : "20",
+                 options : [
+                                  "3",
+                                  "4",
+                                  "5",
+                                  "20"  
+                                  ]
+            
+            },
+           {
+                 number:3,
+                 question: "9×(4+2)÷3",
+                 answer : "18",
+                 options : [ 
+                                  "30",
+                                  "18",
+                                  "15",
+                                  "12"
+                                  ]
+            
+            },
+            {
+                 number:4,
+                 question: "10−(3×2)+5=",
+                 answer : "9",
+                 options : [
+                                  "20",
+                                  "24",
+                                  "30",
+                                  "9"
+                                  ]
+            }, 
+            {
+                 number:5,
+                 question: "16÷(4−1)×2=",
+                 answer : "10.7",
+                 options : [ 
+                                  "8",
+                                  "9",
+                                  "10.7",
+                                  "12"
+                               ]
+            },
+            {
+                 number:6,
+                 question: "20−3÷3+4=",
+                 answer : "23",
+                 options : [
+                                  "2",
+                                  "23",
+                                  "4",
+                                  "5"
+                               ]
+            },
+            {
+                 number:7,
+                 question: "5×(2+3)−4=",
+                 answer : "21",
+                 options : [
+                                  "21",
+                                  "15",
+                                  "9",
+                                  "8"
+                               ]
+            },
+             {
+                 number:8,
+                 question: "36÷(6×2)+3=",
+                 answer : "6",
+                 options : [
+                                 "10",
+                                 "8",
+                                 "6",
+                                 "5"
+                               ]
+            },
+            {
+                 number:9,
+                 question: "12×2−6÷3=",
+                 answer : "22",
+                 options : [
+                                 "22",
+                                 "15",
+                                 "18",
+                                 "20"
+                               ]
+            },
+            {
+               number:10,
+               question: "18−6÷2+5=",
+               answer : "20",
+               options : [
+                               "10",
+                               "20",
+                               "8",
+                               "6"
+                             ]
+          },
+          {
+               number:11,
+               question: " 7×(4−2)+3=",
+               answer : "17",
+               options : [
+                               "17",
+                               "2",
+                               "3",
+                               "4"
+                             ]
+          },
+          {
+               number:12,
+               question: "48÷(4×3)−2=",
+               answer : "2",
+               options : [
+                               "14",
+                               "16",
+                               "18",
+                               "2"
+                             ]
+          },
+          {
+               number:13,
+               question: "15−3×2+4=",
+               answer : "13",
+               options : [
+                               "8",
+                               "13",
+                               "12",
+                               "18"
+                             ]
+          },
+          {
+               number:14,
+               question: " 30÷5×3−2=",
+               answer : "16",
+               options : [
+                               "16",
+                               "35",
+                               "29",
+                               "27"
+                             ]
+          },
+          {
+               number:15,
+               question: "8×(3+2)−7=",
+               answer : "33",
+               options : [
+                               "33",
+                               "16",
+                               "18",
+                               "20"
+                             ]
+          }
+          ,
+          {
+               number:16,
+               question: "45÷(5×2)+3=",
+               answer : "7.5",
+               options : [
+                               "5",
+                               "6",
+                               "7.5",
+                               "8"
+                             ]
+          }
+          ,
+          {
+               number:17,
+               question: " 20−4÷2×3=",
+               answer : "14",
+               options : [
+                               "17",
+                               "13",
+                               "18",
+                               "14"
+                             ]
+          }
+          ,
+          {
+               number:18,
+               question: "12÷3+6×2=",
+               answer : "16",
+               options : [
+                               "7",
+                               "16",
+                               "22",
+                               "20"
+                             ]
+          }
+          ,
+          {
+               number:19,
+               question: "9×(5−3)+4=",
+               answer : "22",
+               options : [
+                               "12",
+                               "14",
+                               "18",
+                               "22"
+                             ]
+          }
+          ,
+          {
+               number:20,
+               question: "64÷(8×2)−3=",
+               answer : "1",
+               options : [
+                               "1",
+                               "2",
+                               "3",
+                               "4"
+                             ]
+          }
+             ];
